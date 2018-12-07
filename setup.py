85089994a0568a3a73f98fe9b92b04323964e0a3
Chadwick Boulay <chadwick.boulay@gmail.com>
2018-12-07 12:46:34 -0500

    Setting up automated builds.

--- setup.py
+++ setup.py
@@ -1,21 +1,37 @@
 """Python setup script for the pylsl distribution package."""
 
-from setuptools import setup, find_packages, Distribution
+from setuptools import setup, Distribution, Extension
+from setuptools.command.install import install
 from codecs import open
 from os import path
 
 
 class BinaryDistribution(Distribution):
-    @staticmethod
-    def has_ext_modules(foo):
+    def has_ext_modules(self):
         return True
+    def is_pure(self):
+        return False
+
+class InstallPlatlib(install):
+    def finalize_options(self):
+        install.finalize_options(self)
+        if self.distribution.has_ext_modules():
+            self.install_lib = self.install_platlib
 
 
 here = path.abspath(path.dirname(__file__))
 
+
 # Get the long description from the relevant file
 with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
+
+#extension_modules = [Extension(
+#    'pylsl.liblsl',
+#    sources=[],
+#    library_dirs=['pylsl/lib'],
+#    libraries=['lsl64']  # TODO: Platform-specific naming?
+#)]
 
 setup(
     name='pylsl',
@@ -23,7 +39,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.12.2',
+    version='1.13.0rc8',
 
     description='Python interface to the Lab Streaming Layer',
     long_description=long_description,
@@ -68,22 +84,25 @@ setup(
 
     # What does your project relate to?
     keywords='networking lsl lab streaming layer labstreaminglayer data acquisition',
+    
+    distclass=BinaryDistribution,
+    cmdclass={'install': InstallPlatlib},
 
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
-    packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
+    packages=['pylsl', 'pylsl.examples'],
 
     # List run-time dependencies here.  These will be installed by pip when
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=[],
+    # install_requires=[],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
     # for example:
     # $ pip install -e .[dev,test]
-    extras_require={},
+    # extras_require={},
 
     # If there are data files included in your packages that need to be
     # installed, specify them here.  If using Python 2.6 or less, then these
@@ -92,24 +111,19 @@ setup(
     # setup will probably only find the one library downloaded by the build
     # script or placed here manually.
     package_data={
-        'pylsl': ['*.dll', '*.dylib', '*.so*'],
+        'pylsl': ['lib/*.so*', 'lib/*.dll', 'lib/*.dylib'],
     },
-
+    
     # Although 'package_data' is the preferred approach, in some case you may
     # need to place data files outside of your packages. See:
     # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files # noqa
     # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
-    data_files=[],
+    # data_files=[],
 
     # To provide executable scripts, use entry points in preference to the
     # "scripts" keyword. Entry points provide cross-platform support and allow
     # pip to create the appropriate form of executable for the target platform.
-    entry_points={},
-
-    # We want to eventually move to having platform-specific distributions
-    # with only the required platform shared objects. Unfortunately, for Linux,
-    # pypi only accepts anylinux builds. Leave the following commented until
-    # anylinux is working with AppVeyor/Travis. This will create the wheel for
-    # 'any' Python. That means we need all available shared objects when we upload.
-#    distclass=BinaryDistribution
+    # entry_points={},
+    #
+    # ext_modules=extension_modules,
 )
