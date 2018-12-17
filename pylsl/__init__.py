1925439d46721978481e5c7b3804f3bed8dd1164
Chadwick Boulay <chadwick.boulay@gmail.com>
2018-12-17 10:40:49 -0500

    Fixes #1

--- pylsl/__init__.py
+++ pylsl/__init__.py
@@ -1,7 +1,7 @@
 # (not using import * for Python 2.5 support)
 from .pylsl import IRREGULAR_RATE, DEDUCED_TIMESTAMP, FOREVER, cf_float32,\
     cf_double64, cf_string, cf_int32, cf_int16, cf_int8, cf_int64,\
-    cf_undefined, protocol_version, library_version, local_clock,\
+    cf_undefined, protocol_version, library_version, library_info, local_clock,\
     proc_ALL, proc_none, proc_clocksync, proc_dejitter, proc_monotonize, proc_threadsafe,\
     StreamInfo, StreamOutlet, resolve_streams, resolve_byprop,\
     resolve_bypred, StreamInlet, XMLElement, ContinuousResolver,\
