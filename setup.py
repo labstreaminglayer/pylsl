f80781d715a7df03a3ebf698e308a30cde3815f2
Chadwick Boulay <chadwick.boulay@gmail.com>
2018-12-17 10:44:52 -0500

    Removed RC version tag from setup.py

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.13.0rc8',
+    version='1.13.0',
 
     description='Python interface to the Lab Streaming Layer',
     long_description=long_description,
