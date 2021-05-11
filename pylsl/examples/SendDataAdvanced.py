0e70dde5870b0cdf13b54ec45fdfa5020869cfce
Chadwick Boulay <chadwick.boulay@gmail.com>
2021-05-11 02:21:03 -0400

    example SendDataAdvanced - some formatting improvements and some guarded code to test a new feature.

--- pylsl/examples/SendDataAdvanced.py
+++ pylsl/examples/SendDataAdvanced.py
@@ -6,13 +6,13 @@ import getopt
 import time
 from random import random as rand
 
-from pylsl import StreamInfo, StreamOutlet, local_clock
+import pylsl
 
 
 def main(argv):
     srate = 100
     name = 'LSLExampleAmp'
-    type = 'EEG'
+    stream_type = 'EEG'
     channel_names = ["C3", "C4", "Cz", "FPz", "POz", "CPz", "O1", "O2"]
     n_channels = len(channel_names)
     help_string = 'SendData.py -s <sampling_rate> -n <stream_name> -t <stream_type>'
@@ -30,14 +30,14 @@ def main(argv):
         elif opt in ("-n", "--name"):
             name = arg
         elif opt in ("-t", "--type"):
-            type = arg
+            stream_type = arg
 
     # first create a new stream info (here we set the name to BioSemi,
     # the content-type to EEG, 8 channels, 100 Hz, and float-valued data) The
     # last value would be the serial number of the device or some other more or
     # less locally unique identifier for the stream as far as available (you
     # could also omit it but interrupted connections wouldn't auto-recover).
-    info = StreamInfo(name, type, n_channels, srate, 'float32', 'myuid2424')
+    info = pylsl.StreamInfo(name, stream_type, n_channels, srate, 'float32', 'myuid2424')
 
     # append some meta-data
     info.desc().append_child_value("manufacturer", "LSLExampleAmp")
@@ -55,13 +55,23 @@ def main(argv):
 
     # next make an outlet; we set the transmission chunk size to 32 samples and
     # the outgoing buffer size to 360 seconds (max.)
-    outlet = StreamOutlet(info, 32, 360)
+    outlet = pylsl.StreamOutlet(info, 32, 360)
+
+    if False:
+        # It's unnecessary to check the info when the stream was created in the same scope; just use info.
+        # Use this code only as a sanity check if you think something when wrong during stream creation.
+        check_info = outlet.get_info()
+        assert check_info.name() == name
+        assert check_info.type() == stream_type
+        assert check_info.channel_count() == len(channel_names)
+        assert check_info.channel_format() == pylsl.cf_float32
+        assert check_info.nominal_srate() == srate
 
     print("now sending data...")
-    start_time = local_clock()
+    start_time = pylsl.local_clock()
     sent_samples = 0
     while True:
-        elapsed_time = local_clock() - start_time
+        elapsed_time = pylsl.local_clock() - start_time
         required_samples = int(srate * elapsed_time) - sent_samples
         if required_samples > 0:
             # make a chunk==array of length required_samples, where each element in the array
@@ -70,8 +80,10 @@ def main(argv):
                        for samp_ix in range(required_samples)]
             # get a time stamp in seconds (we pretend that our samples are actually
             # 125ms old, e.g., as if coming from some external hardware)
-            stamp = local_clock() - 0.125
+            stamp = pylsl.local_clock() - 0.125
             # now send it and wait for a bit
+            # Note that even though `rand()` returns a 64-bit value, the `push_chunk` method
+            #  will convert it to c_float before passing the data to liblsl.
             outlet.push_chunk(mychunk, stamp)
             sent_samples += required_samples
         time.sleep(0.02)
