88a4d35a0d7b34c9ac4bffc6db9033555152b412
Chadwick Boulay <chadwick.boulay@gmail.com>
2021-08-13 11:28:35 -0400

    SendDataAdvanced - Switch to argparse; add channel locations to header.

--- pylsl/examples/SendDataAdvanced.py
+++ pylsl/examples/SendDataAdvanced.py
@@ -1,61 +1,58 @@
 """Example program to demonstrate how to send a multi-channel time-series
 with proper meta-data to LSL."""
-import sys
-import getopt
-
+import argparse
 import time
 from random import random as rand
 
 import pylsl
 
 
-def main(argv):
-    srate = 100
-    name = 'LSLExampleAmp'
-    stream_type = 'EEG'
-    channel_names = ["C3", "C4", "Cz", "FPz", "POz", "CPz", "O1", "O2"]
-    n_channels = len(channel_names)
-    help_string = 'SendData.py -s <sampling_rate> -n <stream_name> -t <stream_type>'
-    try:
-        opts, args = getopt.getopt(argv, "hs:n:t:", longopts=["srate=", "name=", "type"])
-    except getopt.GetoptError:
-        print(help_string)
-        sys.exit(2)
-    for opt, arg in opts:
-        if opt == '-h':
-            print(help_string)
-            sys.exit()
-        elif opt in ("-s", "--srate"):
-            srate = float(arg)
-        elif opt in ("-n", "--name"):
-            name = arg
-        elif opt in ("-t", "--type"):
-            stream_type = arg
+def main(name='LSLExampleAmp', stream_type='EEG', srate=100):
 
-    # first create a new stream info (here we set the name to BioSemi,
-    # the content-type to EEG, 8 channels, 100 Hz, and float-valued data) The
-    # last value would be the serial number of the device or some other more or
-    # less locally unique identifier for the stream as far as available (you
-    # could also omit it but interrupted connections wouldn't auto-recover).
+    channel_names = ["Fp1", "Fp2", "C3", "C4", "Cz", "P3", "P4", "Pz", "O1", "O2"]
+    channel_locations = [
+        [-0.0307, 0.0949, -0.0047],
+        [0.0307, 0.0949, -0.0047],
+        [-0.0742,  4.54343962e-18,  0.0668],
+        [0.0743, 4.54956286e-18, 0.0669],
+        [0, 6.123234e-18, 0.1],
+        [-0.0567, -0.0677,  0.0469],
+        [0.0566, -0.0677,  0.0469],
+        [8.74397815e-18, -0.0714,  0.0699],
+        [-0.0307, -0.0949, -0.0047],
+        [0.0307, -0.0949, -0.0047]
+    ]
+    n_channels = len(channel_names)
+
+    # First create a new stream info.
+    #  The first 4 arguments are stream name, stream type, number of channels, and
+    #  sampling rate -- all parameterized by the keyword arguments or the channel list above.
+    #  For this example, we will always use float32 data so we provide that as the 5th parameter.
+    #  The last value would be the serial number of the device or some other more or
+    #  less locally unique identifier for the stream as far as available (you
+    #  could also omit it but interrupted connections wouldn't auto-recover).
     info = pylsl.StreamInfo(name, stream_type, n_channels, srate, 'float32', 'myuid2424')
 
     # append some meta-data
+    # https://github.com/sccn/xdf/wiki/EEG-Meta-Data
     info.desc().append_child_value("manufacturer", "LSLExampleAmp")
     chns = info.desc().append_child("channels")
-    for label in channel_names:
+    for chan_ix, label in enumerate(channel_names):
         ch = chns.append_child("channel")
         ch.append_child_value("label", label)
         ch.append_child_value("unit", "microvolts")
         ch.append_child_value("type", "EEG")
         ch.append_child_value("scaling_factor", "1")
-    info.desc().append_child_value("manufacturer", "LSLExamples")
+        loc = ch.append_child("location")
+        for ax_str, pos in zip(["X", "Y", "Z"], channel_locations[chan_ix]):
+            loc.append_child_value(ax_str, str(pos))
     cap = info.desc().append_child("cap")
     cap.append_child_value("name", "ComfyCap")
     cap.append_child_value("size", "54")
     cap.append_child_value("labelscheme", "10-20")
 
-    # next make an outlet; we set the transmission chunk size to 32 samples and
-    # the outgoing buffer size to 360 seconds (max.)
+    # next make an outlet; we set the transmission chunk size to 32 samples
+    # and the outgoing buffer size to 360 seconds (max.)
     outlet = pylsl.StreamOutlet(info, 32, 360)
 
     if False:
@@ -79,8 +76,8 @@ def main(argv):
             # is a new random n_channels sample vector
             mychunk = [[rand() for chan_ix in range(n_channels)]
                        for samp_ix in range(required_samples)]
-            # get a time stamp in seconds (we pretend that our samples are actually
-            # 125ms old, e.g., as if coming from some external hardware)
+            # Get a time stamp in seconds. We pretend that our samples are actually
+            # 125ms old, e.g., as if coming from some external hardware with known latency.
             stamp = pylsl.local_clock() - 0.125
             # now send it and wait for a bit
             # Note that even though `rand()` returns a 64-bit value, the `push_chunk` method
@@ -91,4 +88,12 @@ def main(argv):
 
 
 if __name__ == '__main__':
-    main(sys.argv[1:])
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--name', default='LSLExampleAmp',
+                        help="Name of the created stream.")
+    parser.add_argument('--type', default='EEG',
+                        help="Type of the created stream.")
+    parser.add_argument('--srate', default=100.0, help="Sampling rate of the created stream.", type=float)
+    arg = parser.parse_args()
+
+    main(name=arg.name, stream_type=arg.type, srate=arg.srate)
