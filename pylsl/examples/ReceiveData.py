85b367de4be3c8527085779de7e356b9c6e3e2c6
novikov-p-a <45422389+novikov-p-a@users.noreply.github.com>
2022-02-23 13:52:58 +0300

    Update ReceiveData.py
    Arguments were removed from the call of resolve_stream(). With previous arguments, resolve_stream() hangs the thread.

--- pylsl/examples/ReceiveData.py
+++ pylsl/examples/ReceiveData.py
@@ -6,7 +6,7 @@ from pylsl import StreamInlet, resolve_stream
 def main():
     # first resolve an EEG stream on the lab network
     print("looking for an EEG stream...")
-    streams = resolve_stream('type', 'EEG')
+    streams = resolve_stream()
 
     # create a new inlet to read from the stream
     inlet = StreamInlet(streams[0])
