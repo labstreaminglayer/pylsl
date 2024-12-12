fb0ae2ed8a0b6eab3b373a1d614e557ab04c7eec
Chadwick Boulay <chadwick.boulay@gmail.com>
2024-12-12 18:53:05 -0500

    ruff

--- setup.py
+++ setup.py
@@ -11,14 +11,16 @@ from setuptools import setup
 
 try:
     from wheel.bdist_wheel import bdist_wheel as _bdist_wheel
+
     class bdist_wheel(_bdist_wheel):
         def finalize_options(self):
             super().finalize_options()
             self.root_is_pure = not sys.platform.startswith("win")
+
         def get_tag(self):
             python, abi, plat = _bdist_wheel.get_tag(self)
             # We don't contain any python source
-            python, abi = 'py2.py3', 'none'
+            python, abi = "py2.py3", "none"
             return python, abi, plat
 except ImportError:
     bdist_wheel = None
